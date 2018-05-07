a6d430b9e70958fc62c54a8103ca62de3c781a2d
mrdulin <novaline@qq.com>
2018-05-07 10:30:22 +0800

    modify /query router implement

--- src/server.js
+++ src/server.js
@@ -45,11 +45,14 @@ app.post('/query', (req, res) => {
     .detectIntent(request)
     .then(responses => {
       console.log('Detected intent');
-      const result = responses[0].queryResult;
-      console.log(`  Query: ${result.queryText}`);
-      console.log(`  Response: ${result.fulfillmentText}`);
-      if (result.intent) {
-        console.log(`  Intent: ${result.intent.displayName}`);
+      const { queryResult } = responses[0];
+      const { queryText, fulfillmentText, intent } = queryResult;
+      console.log(`  Query: ${queryText}`);
+      console.log(`  Response: ${fulfillmentText}`);
+
+      res.json({ fulfillmentText });
+      if (intent) {
+        console.log(`  Intent: ${intent.displayName}`);
       } else {
         console.log('No intent matched.');
       }
